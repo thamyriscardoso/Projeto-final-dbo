3206354bbe9602c1deccded6aacc2bb55298b8ee
Adrian Pastore <adrianqpastore@gmail.com>
2018-11-28 09:40:15 -0200

    Add files via upload

--- Pagina-inicial/js/progress.js
+++ Pagina-inicial/js/progress.js
@@ -1,32 +1,25 @@
 $(function() {
 
-    var $progress         = $('#progress'), // Barra de Progresso.
-        $progressElements = $('.progress'), // Elementos que devem ser checados
-                                            // para modificar o valor da barra.
-        TOTAL             = $progressElements.length; // Total de elementos.
+    let $progress         = $('#progress');
+    let $progressElements = $('.progress');
+    let TOTAL             = $progressElements.length;
   
     
     $progressElements.on('blur, change', function() {
-      
-      // Faz um filtro com o total elementos válidos.
-      // Nesse caso, campos que não estejam "em branco" e que não estejam marcados
-      // como ':invalid'.
-      var valid = $progressElements.filter(function() {
+      let valid = $progressElements.filter(function() {
         return ($(this).val() || $(this).prop('checked')) && !$(this).is(':invalid');
       }).length;
-      
-      // Calcula a porcentagem e altera o valor da barra.
-      var percent = (valid * 100) / TOTAL,
+      let percent = (valid * 100) / TOTAL,
           current = $progress.val();
       
-      var increase = percent > current;
+      let increase = percent > current;
           
-      var transition = setInterval(function(){
+      let transition = setInterval(function(){
         if((increase && current >= percent) ||
           (!increase && current <= percent))
             clearInterval(transition);
         
-        var value = $progress.val();
+        let value = $progress.val();
         value = increase ? value+1 : value-1;
         current = value;
         
